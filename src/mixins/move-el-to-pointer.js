9968ac212a5d0ee255c6f7e29e74fa74e0bc6d96
Matthew Stokeley <matthewjaestokeley@gmail.com>
2018-12-30 10:13:04 -0500

    Update move-el-to-pointer.js

--- src/mixins/move-el-to-pointer.js
+++ src/mixins/move-el-to-pointer.js
@@ -1,11 +1,12 @@
-function moveElToPointer (event) {
-  var findX = () => event.clientX - event.target.offsetLeft;
-  var findY = () => event.clientY + event.target.offsetTop - event.target.offsetHeight;
+export default function moveElToPointer (event) {
+  // @todo
+  var findX = () => event.clientX - event.target.offsetLeft
+  var findY = () => event.clientY + event.target.offsetTop - event.target.offsetHeight
 
   var transform = (el, x, y) => {
       el.style.transform = "translate("+x+"px, "+y+"px)" 
   }
 
-  transform(event.target, findX(), findY());
+  transform(event.target, findX(), findY())
 
 }
